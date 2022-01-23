27e20df22f15b8199d43d6160191fef729594756
Hans Chaudry <hanschaudry@outlook.com>
2022-01-23 11:54:15 -0500

    everything but the reset button

--- EverupRepo/Projects/Project 3/game.js
+++ EverupRepo/Projects/Project 3/game.js
@@ -1,65 +1,47 @@
+//Elements that will be eddited 
 let ul = document.getElementsByClassName('history');
 let scoreTables = document.getElementsByClassName('points');
 let highScores = document.getElementsByClassName('high');
-let number = Math.ceil(Math.random() * 100);
-//for testing purposes, delete before submitting
-console.log(number);
-let wrongs = 0;
-let high = 0;
 let output = document.getElementById('output');
 let score = document.getElementById('score');
 let main = document.getElementById('main');
 let lose = document.getElementById('lose');
 let win = document.getElementById('win');
 
+//global variables
+let number = Math.ceil(Math.random() * 100);
+//for testing purposes, delete before submitting
+console.log(number);
+let wrongs = 0;
+let high = 0;
+
 
 function check() {
     let input = Number(document.getElementById('input').value);
-
 
     if (isNaN(input)) {
         output.innerHTML = 'Enter a number.';
     }
     
     else if (input === number) {
-        main.style.display = 'none';
-        win.style.display = 'flex';
-        updateHighScore();
+        switchToWin()
     } 
     
-    else if (input > 100) {
-        wrongs +=1;
-        updateScore();
-        output.textContent = 'Your input is out of the 1 to 100 range.';
-        addHistory(input);
+    else if (input > 100 || input > number || input < number) {
+        wrongNumber(input);
         if(wrongs === 10){
-            main.style.display = 'none';
-            lose.style.display = 'flex';
+            switchToLose()
+        }
+        switch (input > number) {
+            case true:
+                output.textContent = 'You\'re guess was too high';
+                break;
+        
+            case false:
+                output.textContent = 'You\'re guess was too low';
+                break;
         }
     }
-    
-    else if (input > number) {
-        wrongs += 1;
-        updateScore();
-        output.textContent = 'You\'re guess was too high';
-        addHistory(input);
-        if(wrongs === 10){
-            main.style.display = 'none';
-            lose.style.display = 'flex';
-        }
-    }
-    
-    else if (input < number) {
-        wrongs += 1;
-        updateScore();
-        output.textContent = 'You\'re guess was too low';
-        addHistory(input);
-        if(wrongs === 10){
-            main.style.display = 'none';
-            lose.style.display = 'flex';
-        }
-    }
-    return false;
 }
 
 function addHistory(num){
@@ -84,9 +66,11 @@ function updateScore(){
 }
 
 function updateHighScore(){
-    for(let n = 0; n < highScores.length; n++){
-        high = 10 - wrongs;
-        highScores[n].innerHTML = `High Score = ${high}`;
+    if((10 - wrongs) > high){
+        for(let n = 0; n < highScores.length; n++){
+            high = 10 - wrongs;
+            highScores[n].innerHTML = `High Score = ${high}`;
+        }
     }
 }
 
@@ -94,9 +78,31 @@ function replay(){
     main.style.display = 'flex';
     lose.style.display = 'none';
     win.style.display = 'none'
+    output.innerHTML = '';
     wrongs = 0;
     number = Math.ceil(Math.random() * 100);
     console.log(`new num ${number}`);
     updateScore();
     clearHistory();
+}
+
+function switchToLose(){
+    main.style.display = 'none';
+    lose.style.display = 'flex';
+    document.getElementById('lost').textContent = `${number}`;
+}
+
+function switchToWin(){
+    main.style.display = 'none';
+    win.style.display = 'flex';
+    updateHighScore();
+    document.getElementById('secret').textContent = `${number}`;
+    document.getElementById('yourScore').textContent = `Your Score = ${10-wrongs}`;
+    document.getElementById('best').textContent = `Best = ${high}`;
+}
+
+function wrongNumber(x){
+    wrongs +=1;
+    updateScore();
+    addHistory(x);
 }
