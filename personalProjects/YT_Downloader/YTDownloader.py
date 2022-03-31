5de06baac673751db6c22dd32adcab545f66c18f
Hans Chaudry <hanschaudry@outlook.com>
2022-03-30 21:26:57 -0400

    new message

--- personalProjects/YT_Downloader/YTDownloader.py
+++ personalProjects/YT_Downloader/YTDownloader.py
@@ -2,75 +2,88 @@ import wx
 import validators
 from pytube import YouTube
 
-class MyFrame(wx.Frame):
+class MainFrame(wx.Frame):
+    # Constructor
     def __init__(self, *args, **kwargs):
-        super(MyFrame, self).__init__(size=(800, 600), *args, **kwargs)
-        self.panel = wx.Panel(self, style=wx.BORDER_SUNKEN)
-        self.url = None
-        self.downloadPath = None
-        self.SetUpGUI()
-        self.resolutions = ["1080p", "720p", "480", "360p", "240p"]
+        super(MainFrame, self).__init__(*args, **kwargs, size=(800, 600))
+
+        #Properties
+        self.screen_ = wx.Panel(self, style=wx.BORDER_SUNKEN)  # Main screen for app
+        self.url_ = None  # link to YouTube video
+        self.downloadPath_ = None  # Destination for video
+        self.resolutions_ = ["1080p", "720p", "480", "360p", "240p"]  # Available resolutions
+
+        self.SetUpGUI()  # Initiates the GUI
 
     def SetUpGUI(self):
         vbox = wx.BoxSizer(wx.VERTICAL)
         vbox.AddStretchSpacer()
 
-        label = wx.StaticText(self.panel, label="YouTube Downloader", style=wx.ALIGN_CENTRE_HORIZONTAL, size=(250, -1))
-        label.SetFont(wx.Font(wx.FontInfo(20)))
-
-        row1 = wx.BoxSizer(wx.VERTICAL)
-        self.url = wx.TextCtrl(self.panel, -1, style=wx.TE_CENTRE, size=(800, 25), pos=(10, 10))
-        vbox.Add(label, 0, wx.CENTER)
-        vbox.Add(self.url, 0, wx.CENTER)
-
+        row1 = wx.BoxSizer(wx.HORIZONTAL)
         row2 = wx.BoxSizer(wx.HORIZONTAL)
-        self.downloadPath = wx.TextCtrl(self.panel, -1, style=wx.TE_CENTRE, size=(700, 25), pos=(10, 10))
-        browse = wx.Button(self.panel, label='Browse')
-        browse.Bind(wx.EVT_BUTTON, self.browseFolders)
-        row2.Add(self.downloadPath, 0)
-        row2.Add(browse, 0)
 
-        download_BTN = wx.Button(self.panel, label='Download', style=wx.ALIGN_CENTER)
-        download_BTN.Bind(wx.EVT_BUTTON, self.downloadVideo)
+        #App Title
+        title = wx.StaticText(self.screen_, label="YouTube Downloader", style=wx.ALIGN_CENTER_HORIZONTAL, size=(250,
+                                                                                                                -1))
+        title.SetFont(wx.Font(wx.FontInfo(20)))
+        vbox.Add(title, 0, wx.CENTER)
 
+        #Everything in row 1
+        label1 = wx.StaticText(self.screen_, label="URL:", style=wx.ALIGN_CENTER_HORIZONTAL)
+        row1.Add(label1, 0, wx.CENTER)
+
+        self.url_ = wx.TextCtrl(self.screen_, -1, style=wx.TE_CENTRE, size=(600, 25), pos=(10, 10))
+        row1.Add(self.url_, 0, wx.CENTER)
         vbox.Add(row1, 0, wx.CENTER)
-        vbox.Add(row2, 0, wx.CENTER)
-        vbox.Add(download_BTN, 0, wx.CENTER)
-        vbox.AddStretchSpacer()
-        self.panel.SetSizer(vbox)
 
+        #Everything in row 2
+        label2 = wx.StaticText(self.screen_, label="Path:", style=wx.ALIGN_LEFT)
+        row2.Add(label2, 0, wx.LEFT)
+
+        self.downloadPath_ = wx.TextCtrl(self.screen_, -1, style=wx.TE_CENTRE, size=(514, 25), pos=(10, 10))
+        row2.Add(self.downloadPath_, 0, wx.CENTER)
+
+        browseBTN = wx.Button(self.screen_, label='Browse')
+        browseBTN.Bind(wx.EVT_BUTTON, self.browseFolders)
+        row2.Add(browseBTN, 0, wx.CENTER)
+        vbox.Add(row2, 0, wx.CENTER)
+
+        #Download Button
+        downloadBTN = wx.Button(self.screen_, label='Download', style=wx.ALIGN_CENTER)
+        downloadBTN.Bind(wx.EVT_BUTTON, self.downloadVideo)
+        vbox.Add(downloadBTN, 0, wx.CENTER)
+
+        vbox.AddStretchSpacer()
+        self.screen_.SetSizer(vbox)
         self.Show()
 
     def browseFolders(self, *args):
-        download_dir = wx.DirDialog(self.panel, style=wx.DD_DEFAULT_STYLE)
+        download_dir = wx.DirDialog(self.screen_, style=wx.DD_DEFAULT_STYLE)
         download_dir.ShowModal()
-        self.downloadPath.Value = download_dir.GetPath()
-        print(download_dir.GetPath())
+        self.downloadPath_.Value = download_dir.GetPath()
 
     def downloadVideo(self, *args):
-        vidLink = self.url.Value
-        print(vidLink)
+        vidLink = self.url_.Value
 
-        if not validators.url(self.url.Value):
+        if not validators.url(self.url_.Value):
             print('invalid url')
         else:
-            folder = self.downloadPath.Value
+            folder = self.downloadPath_.Value
             get_video = YouTube(vidLink)
             get_stream = self.getQualityStream(get_video)
-            print(get_stream)
             get_stream.download(folder)
-            wx.MessageDialog(self.panel, message=("Video was successfully downloaded. The file can be found" 
-                                                  " at "+self.downloadPath.Value)).ShowModal()
+            wx.MessageDialog(self.screen_, message=("Video was successfully downloaded. The file can be found"
+                                                  " at " + self.downloadPath_.Value)).ShowModal()
 
     def getQualityStream(self, link):
-        for reso in self.resolutions:
+        for reso in self.resolutions_:
             streams = link.streams.filter(res=reso, progressive=True)
             if len(streams) != 0:
                 return streams.first()
 
 def main():
     app = wx.App()
-    MyFrame(None, title="DownloadYT")
+    MainFrame(None, title="Practice")
     app.MainLoop()
 
 
